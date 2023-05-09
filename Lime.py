946fb699c8d5ecddb261e64e991c83716ec3e98b
Urfan Alvany <81528157+UrfanAlvany@users.noreply.github.com>
2023-05-09 12:57:55 +0200

    fixed lime

--- Lime.py
+++ Lime.py
@@ -16,6 +16,17 @@ def preprocess_image(img_path, input_shape):
     x = x / 255.0
     return x
 
+def segment_plant(image, lower_hsv, upper_hsv):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_image, lower_hsv, upper_hsv)
+    masked_image = cv2.bitwise_and(image, image, mask=mask)
+    return masked_image, mask
+
+def apply_mask_to_heatmap(heatmap, mask):
+    heatmap_uint8 = (heatmap * 255).astype(np.uint8)
+    masked_heatmap = cv2.bitwise_and(heatmap_uint8, heatmap_uint8, mask=mask)
+    return masked_heatmap
+
 # Create a LIME explainer
 explainer = lime_image.LimeImageExplainer()
 
@@ -27,10 +38,15 @@ def lime_heatmap(input_model, image_array, num_samples=1000, hide_color=None):
     temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
     return mask
 
-input_image_path = '02282fee-ba3f-460e-91e0-630204ab96c2___RS_LB 5031.jpg'
+input_image_path = '04877a47-d7e8-461d-b943-00099f3a2a95___Com.G_TgS_FL 0838.jpg'
 original_image = cv2.imread(input_image_path)
 input_shape = model.input_shape[1:3]
 preprocessed_image = preprocess_image(input_image_path, input_shape)
+
+# Segment the plant from the original image
+lower_hsv = np.array([25, 50, 50])
+upper_hsv = np.array([90, 255, 255])
+segmented_image, plant_mask = segment_plant(original_image, lower_hsv, upper_hsv)
 
 # Classify the image using the model
 prediction = model.predict(preprocessed_image)
@@ -39,10 +55,16 @@ label = 'Sick' if np.argmax(prediction) == 0 else 'Healthy'
 # Apply LIME to generate the heatmap
 heatmap = lime_heatmap(model, preprocessed_image)
 
-# Overlay the heatmap on the original image
+# Resize the plant mask to match the heatmap size
+resized_plant_mask = cv2.resize(plant_mask, (heatmap.shape[1], heatmap.shape[0]))
+
+# Apply the resized plant mask to the LIME heatmap
+masked_heatmap = apply_mask_to_heatmap(heatmap, resized_plant_mask)
+
+# Overlay the masked heatmap on the original image
 original_image_resized = cv2.resize(original_image, input_shape[::-1])  # Resize the image
 original_image_rgb = cv2.cvtColor(original_image_resized, cv2.COLOR_BGR2RGB).astype(np.float32) / 255.0
-overlayed_image = mark_boundaries(original_image_rgb, heatmap).astype(np.float32)
+overlayed_image = mark_boundaries(original_image_rgb, masked_heatmap).astype(np.float32)
 overlayed_image = (overlayed_image * 255).astype(np.uint8)
 
 # Add text to the image
